e8768be0801b355a5a41b8dcb04306cf0f5535ec
Gabriel Eloy <gabrieleloy@Gabriels-MacBook-Pro.local>
2022-01-04 00:01:30 -0300

    ✨ added division feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+interface IItem {
+  id: string;
+  label: string;
+  value: number;
+}
+
 function App() {
+  const totalValueLimit = 100;
+
+  const [items, setItems] = React.useState<IItem[]>([]);
+  const [currentItemLabel, setCurrentItemLabel] = React.useState('');
+
+  const getNewItemsValue = (newItemsCount: number, newItem: IItem) => {
+    const newMedianValue = totalValueLimit/newItemsCount;
+    const oldMedianValue = totalValueLimit/(newItemsCount - 1);
+
+    const newItems = items.map(item => {  
+      const totalPercentage = item.value / oldMedianValue;
+      const newValue = newMedianValue * totalPercentage;
+      return {...item, value: newValue}
+    })
+
+    newItems.push({...newItem, value: newMedianValue});
+
+    return newItems;
+  }
+  
+  const handleInsertItem = () => {
+    const newItem = {
+      id: `item-${items.length}_${Math.ceil(Math.random() * 10000)}`,
+      label: currentItemLabel,
+      value: 0
+    }
+
+    const newItems = getNewItemsValue(items.length + 1, newItem);
+    console.log({newItems})
+    setItems(newItems)
+    setCurrentItemLabel('');
+  }
+
+  const onUpdateItem = (id: string, newValue: string) => {
+    const oldValue = items.find(item => item.id === id)?.value as number;
+    const oldDiff = totalValueLimit - oldValue;
+    
+    const newDiff = totalValueLimit - Number(newValue);
+
+    const newItems = items.map(item => {
+      if(item.id === id){
+        return {...item, value: Number(newValue)}
+      }
+      
+      const itemPercentage = item.value / oldDiff;
+      const newItemValue = newDiff * itemPercentage;
+
+      return {...item, value: newItemValue}
+    })
+
+    setItems(newItems);
+  }
+
+  console.log(items)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={currentItemLabel} onChange={(e) => setCurrentItemLabel(e.currentTarget.value)} />
+      <button onClick={handleInsertItem}>insert new item</button>
+
+      <div>
+        <ul>
+          {items.map(item => (<li key={item.id}><span>{item.label}</span>
+          <input type="range" onChange={e => onUpdateItem(item.id,e.currentTarget.value)} value={item.value}/><span>{item.value.toFixed(2)}</span></li>))}
+        </ul>
+      </div>
     </div>
   );
 }
